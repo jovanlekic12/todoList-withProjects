6a50dbce29e9e8634c33f3d32ab2365aeae76649
jovanlekic12 <lekicjovan1212@gmail.com>
2024-08-01 01:14:58 +0200

    add todo class

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,21 @@
-import './style.css'
+"use strict";
+
+import "./style.css";
+
+const projectAddBtn = document.querySelector(".projects-bar-add");
+const projectCancelBtn = document.querySelector(".projects-form-cancel-btn");
+const todoAddBtn = document.querySelector(".todos-bar-add");
+const todoCancelBtn = document.querySelector(".todos-form-cancel-btn");
+const projectForm = document.querySelector(".projects-form");
+const todoForm = document.querySelector(".todos-bar-form");
+
+class Todo {
+  id;
+  name;
+  date;
+  constructor(name, date) {
+    this.id = self.crypto.randomUUID();
+    this.name = name;
+    this.date = date;
+  }
+}
